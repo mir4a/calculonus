3b1bc8b8c1556cab6f69661d4e309c3147d8bdd9
Miroslav Martynov <marty.mircha@gmail.com>
2014-06-19 11:15:06 +0300

    #1: Removed some func.

--- app/scripts/controllers/calculus.js
+++ app/scripts/controllers/calculus.js
@@ -19,36 +19,6 @@ angular.module('calculonusApp')
         ar = t.split(' ');
         return ar;
       };
-      this.findCaret = function(text) {
-        var t = text || this.text;
-        var pat = new RegExp('\n');
-        if (pat.test(t)) {
-          console.log('yes it has caret return');
-          this.rows = t.split('\n');
-        } else {
-          console.log('stil nothing');
-        }
-      };
-      this.oneSymbolHighlight = function(text) {
-        var t = text || this.rows;
-        if (t.length > 0) {
-          var wrappedText = '';
-          for (var i=0;i<t.length;i++) {
-            console.log(t[i]);
-            for (var j=0; j < t[i].length; i++) {
-              if (t[i][j].length === 1) {
-                wrappedText += '<span class="red">' + t[i][j] + '</span> ';
-              } else {
-                wrappedText += t[i][j] + ' ';
-              }
-            }
-          }
-          // wrappedText += '<span class="red">' + t + '</span> ';
-          return wrappedText;
-        } else {
-          return 0;
-        }
-      };
     }
 
     var calc1 = new Calculus();
